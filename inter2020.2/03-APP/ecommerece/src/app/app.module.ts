25fa92d147d4ce3229aaeae1cdc96acc55408b5e
Armando <rivera.armando997@gmail.com>
2020-08-18 22:43:02 -0500

    Krishwait cards and category products

--- inter2020.2/03-APP/ecommerece/src/app/app.module.ts
+++ inter2020.2/03-APP/ecommerece/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProductCardComponent } from './shared/product-card/product-card.component';
+import { CategoryProductsComponent } from './products/components/category-products/category-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    CategoryProductsComponent
   ],
   imports: [
     BrowserModule,
