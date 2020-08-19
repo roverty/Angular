41b06992593bc15f99b327cc482767e909b458d3
Armando <rivera.armando997@gmail.com>
2020-08-19 01:40:40 -0500

    Krishwait header notification and loading

--- inter2020.2/03-APP/ecommerece/src/app/shared/product-card/product-card.component.ts
+++ inter2020.2/03-APP/ecommerece/src/app/shared/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
+
+  @Input() product;
 
   constructor() { }
 
