35fb6a0001e93447e7f4f4a2828d505a856893da
Armando <rivera.armando997@gmail.com>
2020-08-21 01:20:10 -0500

    Krishwait Maquetacion y backend para hacer post

--- inter2020.2/03-APP/Backend/functions/src/index.ts
+++ inter2020.2/03-APP/Backend/functions/src/index.ts
@@ -6,6 +6,14 @@ import * as cors from 'cors';
 
 const serviceAccount = require('./serviceAccountKey.json');
 
+interface Product {
+    id: string;
+    title: string;
+    price: number;
+    description: string;
+    img: string;
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://krishwait-3933a.firebaseio.com"
@@ -33,7 +41,7 @@ app.get('/getProduct', async (req: any, res: any) => {
 	const id = req.query.idProduct || 0;
 	const productRef = db.collection('products');
 	const productSnap = await productRef.get();
-	const productId = productSnap.docs.find(product => product.id === id);
+	const productId = productSnap.docs.find(product => product.data().id === id);
 
 	let product;
 
@@ -50,6 +58,25 @@ app.get('/getProduct', async (req: any, res: any) => {
 	res.json(
 		product
 	);
+});
+
+app.post('/postProduct', async(req: any, res: any) => {
+	console.log(req.body);
+	const myObject: Product = req.body;
+
+	if (myObject.id || myObject.title || myObject.price || myObject.img) {
+		await db.collection('products').add(req.body);
+		res.status(201).json({
+			error: false,
+			message: 'Success'
+		});
+	} else {
+		res.status(400).json({
+			error: true,
+			message: 'Bad request'
+		});
+	}
+
 });
 
 export const api = functions.https.onRequest(app);
